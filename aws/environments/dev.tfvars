169f443f2779c9a17d60e4ac00936eefbcd154e7
Rivera-py <jaluri@outlook.com>
2025-11-01 14:17:02 +0000

    refactor(cmk): Switch KMS key to shared for whole environment

--- aws/environments/dev.tfvars
+++ aws/environments/dev.tfvars
@@ -2,6 +2,7 @@ eks_capacity_type       = "SPOT"
 node_group_desired_size = 2
 node_group_max_size     = 2
 efs_ia_policy           = "AFTER_1_DAY"
+enable_efs_cmk_encryption = false
 use_private_cidrs       = false
 node_group_ami_type     = "AL2023_x86_64_STANDARD"
 node_group_instance_types = [
--- aws/logging.tf
+++ aws/logging.tf
@@ -1,48 +1,3 @@
-# Shared Logging Resources
-
-data "aws_iam_policy_document" "cloudwatch_logs_kms" {
-  statement {
-    sid    = "AllowCloudWatchLogs"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["logs.${data.aws_region.current.region}.amazonaws.com"]
-    }
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey"
-    ]
-    resources = ["*"]
-  }
-  statement {
-    sid    = "AllowAccount"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_caller_identity.current.account_id]
-    }
-    actions = [
-      "kms:*"
-    ]
-    resources = ["*"]
-  }
-}
-
-resource "aws_kms_key" "cloudwatch_logs" {
-  count               = var.enable_cloudwatch_logging ? 1 : 0
-  description         = "KMS key for cloudwatch log group encryption - Managed by Terraform"
-  enable_key_rotation = true
-
-  policy = data.aws_iam_policy_document.cloudwatch_logs_kms.json
-
-  tags = {
-    Name = "${local.environment}-cloudwatch-logs-kms"
-  }
-}
-
 # VPC Flow Logs resources
 
 data "aws_iam_policy_document" "vpc_flow_logs_assume_role" {
@@ -94,7 +49,7 @@ resource "aws_cloudwatch_log_group" "eks_vpc_flow_logs" {
   count             = var.enable_cloudwatch_logging ? 1 : 0
   name              = "/aws/vpc/${local.environment}-eks-flow-logs"
   retention_in_days = 14
-  kms_key_id        = aws_kms_key.cloudwatch_logs[0].arn
+  kms_key_id        = aws_kms_key.customer_managed_key[0].arn
 
   tags = {
     Name = "${local.environment}-eks-vpc-flow-logs"
@@ -120,7 +75,7 @@ resource "aws_cloudwatch_log_group" "eks_control_plane" {
   count             = var.enable_cloudwatch_logging ? 1 : 0
   name              = "/aws/eks/eks-cluster-${local.environment}/cluster"
   retention_in_days = 14
-  kms_key_id        = aws_kms_key.cloudwatch_logs[0].arn
+  kms_key_id        = aws_kms_key.customer_managed_key[0].arn
 
   tags = {
     Name = "${local.environment}-eks-control-plane-logs"
